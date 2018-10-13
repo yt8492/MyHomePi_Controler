d11ee5b89697646c9dd8389d8731950e2dfc2a97
to <>
2018-10-13 11:51:08 +0900

    Initial commit

---
+++ app/src/main/java/com/yt8492/androidthingscontroler/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.yt8492.androidthingscontroler
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    var switchStatus = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val db = FirebaseFirestore.getInstance()
+        switch1.setOnCheckedChangeListener { _, isChecked ->
+            switchStatus = isChecked
+        }
+        button.setOnClickListener {
+            val segmentNum = segmentValue.text.toString().toIntOrNull()
+            val status = mapOf<String, Any?>(
+                    "led" to switchStatus,
+                    "segment" to segmentNum
+            )
+            db.collection("status").document(Date().time.toString())
+                    .set(status)
+                    .addOnSuccessListener {
+                        toast("Success!")
+                    }
+                    .addOnFailureListener {
+                        toast("Failed")
+                    }
+        }
+    }
+
+    fun toast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+}
